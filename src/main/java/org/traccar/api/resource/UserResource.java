d2dea9106489d9186aaff25c334182ae9fe48de8
Ahmed Ali Rashid <ahmedjadd40@gmail.com>
2019-09-09 16:13:10 +0300

    Abstracted the auto subscription method to a utitlity class

--- src/main/java/org/traccar/api/resource/UserResource.java
+++ src/main/java/org/traccar/api/resource/UserResource.java
@@ -19,11 +19,9 @@ import org.traccar.Context;
 import org.traccar.api.BaseObjectResource;
 import org.traccar.database.UsersManager;
 import org.traccar.helper.LogAction;
+import org.traccar.helper.TrackBeesUtil;
 import org.traccar.model.ManagedUser;
 import org.traccar.model.User;
-import org.traccar.model.Notification;
-import org.traccar.model.Event;
-import org.traccar.model.Typed;
 
 import javax.annotation.security.PermitAll;
 import javax.ws.rs.Consumes;
@@ -38,6 +36,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Set;
+
 
 @Path("users")
 @Produces(MediaType.APPLICATION_JSON)
@@ -86,48 +85,14 @@ public class UserResource extends BaseObjectResource<User> {
         }
         Context.getUsersManager().addItem(entity);
 
-        subscribeToNotifications(entity.getId());
+        TrackBeesUtil.subscribeToNotifications(entity.getId());
 
         LogAction.create(getUserId(), entity);
         if (Context.getPermissionsManager().getUserManager(getUserId())) {
             Context.getDataManager().linkObject(User.class, getUserId(), ManagedUser.class, entity.getId(), true);
             LogAction.link(getUserId(), User.class, getUserId(), ManagedUser.class, entity.getId());
         }
-        Context.getNotificationManager().refreshItems();
         Context.getUsersManager().refreshUserItems();
         return Response.ok(entity).build();
-    }
-
-    private void subscribeToNotifications(long userId) throws SQLException {
-        String commaSeparatedNotificatorTypes =
-                getCommaSeparatedNotificatorTypes(Context.getNotificatorManager().getAllNotificatorTypes());
-        createAndLinkNotification(Event.TYPE_HIGH_TEMPERATURE, userId, commaSeparatedNotificatorTypes);
-        createAndLinkNotification(Event.TYPE_LOW_TEMPERATURE, userId, commaSeparatedNotificatorTypes);
-        createAndLinkNotification(Event.TYPE_HIGH_HUMIDITY, userId, commaSeparatedNotificatorTypes);
-        createAndLinkNotification(Event.TYPE_LOW_HUMIDITY, userId, commaSeparatedNotificatorTypes);
-    }
-
-    private void createAndLinkNotification(String eventType, long userId, String commaSeparatedNotificatorTypes)
-            throws SQLException {
-        Notification notification = new Notification();
-        notification.setAlways(true);
-        notification.setNotificators(commaSeparatedNotificatorTypes);
-        notification.setType(eventType);
-        notification.setCalendarId(0L);
-        notification.setId(-1L);
-        Context.getNotificationManager().addItem(notification);
-        Context.getDataManager().linkObject(User.class, userId, Notification.class, notification.getId(), true);
-        LogAction.link(userId, User.class, userId, Notification.class, notification.getId());
-    }
-
-    private String getCommaSeparatedNotificatorTypes(Set<Typed> typedSet) {
-        String commaSeparatedNotificatorTypes = "";
-        for (Typed typed : typedSet) {
-            commaSeparatedNotificatorTypes = commaSeparatedNotificatorTypes.concat(typed.getType() + ",");
-        }
-        if (!commaSeparatedNotificatorTypes.endsWith(",")) {
-            return commaSeparatedNotificatorTypes;
-        }
-        return commaSeparatedNotificatorTypes.substring(0, commaSeparatedNotificatorTypes.length() - 1);
     }
 }
---
+++ src/main/java/org/traccar/helper/TrackBeesUtil.java
@@ -0,0 +1,53 @@
+package org.traccar.helper;
+
+import org.traccar.Context;
+import org.traccar.model.Event;
+import org.traccar.model.Notification;
+import org.traccar.model.Typed;
+import org.traccar.model.User;
+
+import java.sql.SQLException;
+import java.util.Set;
+
+/**
+ * @author Ahmed Ali Rashid
+ */
+
+public final class TrackBeesUtil {
+
+    private TrackBeesUtil() {
+    }
+
+    public static void subscribeToNotifications(long userId) throws SQLException {
+        String commaSeparatedNotificatorTypes =
+                getCommaSeparatedNotificatorTypes(Context.getNotificatorManager().getAllNotificatorTypes());
+        createAndLinkNotification(Event.TYPE_HIGH_TEMPERATURE, userId, commaSeparatedNotificatorTypes);
+        createAndLinkNotification(Event.TYPE_LOW_TEMPERATURE, userId, commaSeparatedNotificatorTypes);
+        createAndLinkNotification(Event.TYPE_HIGH_HUMIDITY, userId, commaSeparatedNotificatorTypes);
+        createAndLinkNotification(Event.TYPE_LOW_HUMIDITY, userId, commaSeparatedNotificatorTypes);
+
+        Context.getNotificationManager().refreshItems();
+    }
+    private static String getCommaSeparatedNotificatorTypes(Set<Typed> typedSet) {
+        String commaSeparatedNotificatorTypes = "";
+        for (Typed typed : typedSet) {
+            commaSeparatedNotificatorTypes = commaSeparatedNotificatorTypes.concat(typed.getType() + ",");
+        }
+        if (!commaSeparatedNotificatorTypes.endsWith(",")) {
+            return commaSeparatedNotificatorTypes;
+        }
+        return commaSeparatedNotificatorTypes.substring(0, commaSeparatedNotificatorTypes.length() - 1);
+    }
+    private static void createAndLinkNotification(String eventType, long userId, String commaSeparatedNotificatorTypes)
+            throws SQLException {
+        Notification notification = new Notification();
+        notification.setAlways(true);
+        notification.setNotificators(commaSeparatedNotificatorTypes);
+        notification.setType(eventType);
+        notification.setCalendarId(0L);
+        notification.setId(-1L);
+        Context.getNotificationManager().addItem(notification);
+        Context.getDataManager().linkObject(User.class, userId, Notification.class, notification.getId(), true);
+        LogAction.link(userId, User.class, userId, Notification.class, notification.getId());
+    }
+}
